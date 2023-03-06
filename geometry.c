a04a35aba402c66255e30f3a043c2c090a9781e5
Zhur Andrew <gosharak12345@mail.ru>
2023-03-06 21:11:08 +0700

    Add take and show info circle and add 2 new functions

--- geometry.c
+++ geometry.c
@@ -26,6 +26,7 @@ enum Errors {
     ER_EXPECT_COMMA,
     ER_UNEXPECT_COMMA,
 };
+
 void print_error(int column, int status)
 {
     for (int i = 0; i < column; i++) {
@@ -61,6 +62,7 @@ void print_error(int column, int status)
                column);
     }
 }
+
 void del_space(int* column, FILE* file)
 {
     char ch;
@@ -131,6 +133,7 @@ double get_number(int* column, FILE* file)
     char* eptr;
     return strtod(temp, &eptr);
 }
+
 bool expect(char expect, int* column, int status, FILE* file)
 {
     char ch;
@@ -153,3 +156,88 @@ bool unexpect(char unexpect, int* column, int status, FILE* file)
     return true;
 }
 
+void get_point(Point* point, int* column, FILE* file)
+{
+    point->x = get_number(column, file);
+    unexpect(',', column, ER_UNEXPECT_COMMA, file);
+
+    point->y = get_number(column, file);
+}
+
+void end_of_line(int* column, FILE* file)
+{
+    char ch;
+    while ((ch = getc(file)) != '\n' && ch != EOF) {
+        if (ch != ' ') {
+            print_error(*column, ER_UNEXPECT_TOKEN);
+            exit(EXIT_FAILURE);
+        }
+        *column += 1;
+    }
+}
+
+void take_info_circle(Circle* circle, int* column, FILE* file)
+{
+        get_point(&circle->point, column, file);
+        expect(',', column, ER_EXPECT_COMMA, file);
+
+        circle->raduis = get_number(column, file);
+
+        expect(')', column, ER_BACK_BRACE, file);
+
+        end_of_line(column, file);
+    
+    circle->perimeter = 2 * M_PI * circle->raduis;
+    circle->area = M_PI * circle->raduis * circle->raduis;
+}
+
+void show_info_circle(Circle* circle)
+{
+    printf("circle(%.2f %.2f, %.2f)\n",
+           circle->point.x,
+           circle->point.y,
+           circle->raduis);
+    printf("\tarea = %.4f\n", circle->area);
+    printf("\tperimeter = %.4f\n", circle->perimeter);
+}
+
+void parser_stdin(FILE* stdin)
+{
+    char geom[NAME_SIZE] = {0};
+    char ch;
+    int column;
+
+    puts("Enter a geometric shape (or q for exit):");
+    while ((ch = getc(stdin)) != EOF && ch != 'q') {
+        column = 0;
+        do {
+            if (ch == '(' || ch == ' ') {
+                if (strcmp(geom, "circle") == 0) {
+                    Circle circle;
+                    take_info_circle(&circle, &column, stdin);
+                    printf("\nYou have entered: \n");
+                    show_info_circle(&circle);
+                    break;
+                } else {
+                    print_error(0, ER_NAME);
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            if (ch == ')') {
+                print_error(column, ER_BACK_BRACE);
+                exit(EXIT_FAILURE);
+            }
+
+            geom[column++] = ch;
+
+        } while ((ch = getc(stdin)) != '\n');
+    }
+}
+
+int main()
+{
+    parser_stdn(stdin);
+
+    return 0;
+}
