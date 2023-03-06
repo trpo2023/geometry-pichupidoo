e10df4ebe0354f22022aa7af6411920a86009d94
Zhur Andrew <gosharak12345@mail.ru>
2023-03-06 21:00:49 +0700

    Add errors print

--- geometry.c
+++ geometry.c
@@ -26,3 +26,39 @@ enum Errors {
     ER_EXPECT_COMMA,
     ER_UNEXPECT_COMMA,
 };
+void print_error(int column, int status)
+{
+    for (int i = 0; i < column; i++) {
+        putchar(' ');
+    }
+    printf("^\n");
+    switch (status) {
+    case ER_NAME:
+        printf("Error at column %d: expected "
+               "'circle'\n",
+               column);
+        break;
+    case ER_NOT_DOUBLE:
+        printf("Error at column %d: expected "
+               "'<double>'\n",
+               column);
+        break;
+    case ER_BACK_BRACE:
+        printf("Error at column %d: expected ')'\n",
+               column);
+        break;
+    case ER_UNEXPECT_TOKEN:
+        printf("Error at column %d: expected "
+               "token\n",
+               column);
+        break;
+    case ER_EXPECT_COMMA:
+        printf("Error at column %d: expected ','\n",
+                column);
+        break;
+    case ER_UNEXPECT_COMMA:
+        printf("Error at column %d: expected ','\n",
+               column);
+    }
+}
+
